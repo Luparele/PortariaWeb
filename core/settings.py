326bce96b0f194b6ef2a6beb4b88dc10b79fb980
Luparele <eduardo.luparele@gmail.com>
2026-04-09 15:07:42 -0300

    Deployment: Configuração de ALLOWED_HOSTS para o domínio de produção

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '[REDACTED_SECRET_KEY]'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['checkintalog.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
